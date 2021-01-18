5ea49337305442203c9f8dffa07127d1b5a56d25
Ivan Zlatanov <ivanff1@abv.bg>
2021-01-18 17:13:39 +0200

    Version was updated to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setuptools.setup(
     name='pythod',
-    version='0.0.1',
+    version='0.0.2',
     author='Ivan Zlatanov',
     author_email='i_zlatanov@protonmail.com',
     description='Pythod is a lightweigh Python directory organizing CLI tool.',
